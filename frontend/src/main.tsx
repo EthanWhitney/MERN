63063c811194a360f433cb24aadf20fb2fa66bb4
MaxJones2003 <jones.max2003@gmail.com>
2026-04-13 13:13:37 -0400

    mobile browser support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/responsive.css'
+import './styles/modals-responsive.css'
+import './styles/touch-targets.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
